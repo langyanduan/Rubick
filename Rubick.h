7d61154dae3bb0d3a39601da233aebb52c6b6cab
wufan <wufan@dacai.com>
2016-09-20 16:04:33 +0800

    add min os version detect

--- Rubick.h
+++ Rubick.h
@@ -6,6 +6,10 @@
 //
 //
 
+#if __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_8_0
+#error "Rubick uses features only available in iOS SDK 8.0 and later."
+#endif
+
 #import <UIKit/UIKit.h>
 
 //! Project version number for Rubick.
@@ -15,5 +19,3 @@ FOUNDATION_EXPORT double RubickVersionNumber;
 FOUNDATION_EXPORT const unsigned char RubickVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Rubick/PublicHeader.h>
-
-
