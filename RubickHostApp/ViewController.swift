50f5aa2c5a7754e4fadf630e2e792f1c6ad09012
wufan <wufan@dacai.com>
2016-09-27 14:55:35 +0800

    add rubick host app

---
+++ RubickHostApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RubickHostApp
+//
+//  Created by WuFan on 2016/9/27.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
