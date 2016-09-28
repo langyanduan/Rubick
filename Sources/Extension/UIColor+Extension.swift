68a2f50ed07156ac33709bfb2c4c59095280c259
sxf <347773894@qq.com>
2016-09-28 18:43:53 +0800

    1.方案详情，赛事和心水分布 网络数据 2.历史心水完善 3.bug修复

--- Sources/Extension/UIColor+Extension.swift
+++ Sources/Extension/UIColor+Extension.swift
@@ -15,6 +15,29 @@ public func UIColorFromRGB(_ rgbHex: UInt32) -> UIColor {
     return UIColor(red: r / 255, green: g / 255, blue: b / 255, alpha: 1)
 }
 
+//字符串转为颜色  sxf todo
+public func UIColorFromHexString(_ hexString: NSString) -> UIColor {
+     var cString:NSString = hexString.trimmingCharacters(in: NSCharacterSet.whitespacesAndNewlines).uppercased() as NSString
+    if (cString.hasPrefix("#")) {
+        cString = (cString as NSString).substring(from: 1) as NSString
+    }
+    if (cString.length != 6) {
+        return UIColor.gray
+    }
+    
+    var rString = (cString as NSString).substring(to: 2)
+    var gString = ((cString as NSString).substring(from: 2) as NSString).substring(to: 2)
+    var bString = ((cString as NSString).substring(from: 4) as NSString).substring(to: 2)
+    
+    var r:CUnsignedInt = 0, g:CUnsignedInt = 0, b:CUnsignedInt = 0;
+    Scanner(string: rString).scanHexInt32(&r)
+    Scanner(string: gString).scanHexInt32(&g)
+    Scanner(string: bString).scanHexInt32(&b)
+    
+    
+    return UIColor(red: CGFloat(r) / 255.0, green: CGFloat(g) / 255.0, blue: CGFloat(b) / 255.0, alpha: CGFloat(1))
+}
+
 public func UIColorGetComponents(_ color: UIColor) -> [CGFloat] {
     if color == UIColor.black {
         return [ 0, 0, 0, 1 ]
